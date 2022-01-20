d2362c60aa5494dfcee8866e51d30d0c9a0e1d3f
Paulo JRN <paulo.xcco@gmail.com>
2022-01-20 14:31:40 -0200

    Adição de ferramentas para qualidade de código somente para ambiente de desenvolvimento

--- README.md
+++ README.md
@@ -8,3 +8,15 @@
 06. docker run --rm --interactive --tty --volume $PWD:/app composer require symfony/css-selector
 07. docker run --rm --interactive --tty --volume $PWD:/var/www/html --workdir=/var/www/html/ php php init.php
 08. docker run --rm --interactive --tty --volume $PWD:/app composer dump-autoload
+09. docker run --rm --interactive --tty --volume $PWD:/app composer require --dev phpunit/phpunit
+10. docker run --rm --interactive --tty --volume $PWD:/var/www/html --workdir=/var/www/html/ php php vendor/bin/phpunit src/TrackerTest.php
+11. composer install --no-dev -> não instalar itens marcados como --dev (apenas para desenvolvimento)
+12. https://github.com/squizlabs/PHP_CodeSniffer
+13. docker run --rm --interactive --tty --volume $PWD:/app composer require --dev squizlabs/php_codesniffer
+14. docker run --rm --interactive --tty --volume $PWD:/var/www/html --workdir=/var/www/html/ php php vendor/bin/phpcs --help
+15. docker run --rm --interactive --tty --volume $PWD:/var/www/html --workdir=/var/www/html/ php php vendor/bin/phpcs --standard=PSR12 src
+16. https://github.com/phan/phan
+17. docker run --rm --interactive --tty --volume $PWD:/app composer require --dev phan/phan
+18. docker run --rm --interactive --tty --volume $PWD:/var/www/html --workdir=/var/www/html/ php php vendor/bin/phan --help
+19. docker run --rm --interactive --tty --volume $PWD:/var/www/html --workdir=/var/www/html/ php php vendor/bin/phan --allow-polyfill-parser src/Tracker.php
+20. Após a criação do arquivo .phan/config.php: docker run --rm --interactive --tty --volume $PWD:/var/www/html --workdir=/var/www/html/ php php vendor/bin/phan --allow-polyfill-parser
---
+++ src/TrackerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Alura\Tracker\TrackerTest;
+
+use Alura\Tracker\Tracker;
+use GuzzleHttp\ClientInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class TrackerTest extends TestCase
+{
+    private $httpClientMock;
+    private $url = 'url-teste';
+
+    protected function setUp(): void
+    {
+        /**
+         * @var MockObject $stream
+         */
+
+        $html = <<<END
+        <html>
+            <body>
+                <span class="card-curso__nome">Curso Teste 1</span>
+                <span class="card-curso__nome">Curso Teste 2</span>
+                <span class="card-curso__nome">Curso Teste 3</span>
+            </body>
+        </html>
+        END;
+
+
+        $stream = $this->createMock(StreamInterface::class);
+        $stream->expects($this->once())
+            ->method('__toString')
+            ->willReturn($html);
+
+        $response = $this->createMock(ResponseInterface::class);
+        $response->expects($this->once())
+            ->method('getBody')
+            ->willReturn($stream);
+
+        $httpClient = $this->createMock(ClientInterface::class);
+        $httpClient->expects($this->once())
+            ->method('request')
+            ->with('GET', $this->url)
+            ->willReturn($response);
+
+        $this->httpClientMock = $httpClient;
+    }
+
+    public function testTrackerCourses()
+    {
+        $crawler = new Crawler();
+        $tracker = new Tracker($this->httpClientMock, $crawler);
+        $courses = $tracker->search($this->url);
+
+        $this->assertCount(3, $courses);
+        $this->assertEquals('Curso Teste 1', $courses[0]);
+        $this->assertEquals('Curso Teste 2', $courses[1]);
+        $this->assertEquals('Curso Teste 3', $courses[2]);
+    }
+}
